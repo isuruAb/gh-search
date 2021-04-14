015569c96eb655c618f848a52ef69c171bd3c534
isuruAb <vpisabeywardana@gmail.com>
2021-04-14 11:41:00 +0800

    install node-sacss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import styles from "./App.scss";
 
-function App() {
+function App({ onChange, value, optionsArray }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={value} onChange={onChange} />
+      <div className={`${styles.dropdownOptions} position-absolute`}>
+        {optionsArray.map((data) => {
+          return <ul name={data.key}>{data.label}</ul>;
+        })}
+      </div>
     </div>
   );
 }
