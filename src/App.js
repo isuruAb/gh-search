4a80b42a847b83ac88adac1197f52d61c57b05c8
isuruAb <vpisabeywardana@gmail.com>
2021-04-14 14:03:32 +0800

    debounce api call to avoid exceeding limit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { useState,  } from "react";
+import { useState, useEffect } from "react";
+import styles from "./app.module.scss";
 import DropdownOptions from "./components/DropdownOptions";
+import { getData } from "./utils/api";
 
 function App({ onChange, value, optionsArray }) {
   const [suggestions, setSuggestions] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
- 
+
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
     onChange(e);
   };
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(async () => {
+      if (searchTerm) {
+        const params = [{ key: "q", value: searchTerm }];
+        const searchResult = await getData({ params });
+        setSuggestions(searchResult?.items);
+      }
+    }, 1000);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm]);
 
   return (
     <div>
